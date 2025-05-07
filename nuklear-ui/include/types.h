de51a656858a67224b19ba00cbd904245561d8bb
josephclydemar <clydemar234@gmail.com>
2025-05-07 13:06:07 +0800

    Add nuklear-ui

---
+++ nuklear-ui/include/types.h
@@ -0,0 +1,14 @@
+#ifndef TYPES_H_
+#define TYPES_H_
+
+#include <stddef.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct {
+    int32_t x, y;
+} vec2_t;
+
+#endif // TYPES_H_
---
+++ nuklear-ui/include/window.h
@@ -0,0 +1,9 @@
+#ifndef WINDOW_H
+#define WINDOW_H
+
+#define WINDOW_WIDTH   1270
+#define WINDOW_HEIGHT  584
+
+
+#endif // WINDOW_H
+
